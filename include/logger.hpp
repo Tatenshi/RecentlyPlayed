822cf24d2746d78525452c384acf218ecb9f2a7d
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-19 19:08:44 -0600

    Bump for 1.40.2

--- include/logger.hpp
+++ include/logger.hpp
@@ -5,7 +5,7 @@
 
 // Include the modloader header, which allows us to tell the modloader which mod this is, and the version etc.
 #include "scotland2/shared/loader.hpp"
-#include "paper/shared/logger.hpp"
+#include "paper2_scotland2/shared/logger.hpp"
 
 // Static Variables
 inline modloader::ModInfo modInfo = {MOD_ID, VERSION, 0}; // Stores the ID and version of our mod, and is sent to the modloader upon startup
